e2d25a09bc0dcf4c927064ec1b830313216ddaac
renato <renatonytadu@gmail.com>
2025-12-20 07:25:37 +0300

    Prepare for Render deployment

--- final/server.js
+++ final/server.js
@@ -9,7 +9,7 @@ const User = require('./Models/User');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const router = express.Router();
 const CHAPA_INIT_URL = "https://api.chapa.co/v1/transaction/initialize";
